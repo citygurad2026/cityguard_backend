19954f8bea1a480aa371e2b1a41cd3e5801b4267
spectrumexperice <spectrumweb22@gmail.com>
2026-02-09 19:50:03 +0300

    updated

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://guard-sand.vercel.app",
+   process.env.APP_URL || "https://guard-sand.vercel.app",
 ];
 
 
